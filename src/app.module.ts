ba677585bbad79715f1f952cb55a2d00f10b900c
Diego Hideky <diegohideky@gmail.com>
2022-01-25 17:29:55 -0300

    Added crud of users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/school')],
+  imports: [TasksModule, MongooseModule.forRoot('mongodb://localhost/school'), UsersModule],
   controllers: [],
   providers: [],
 })
