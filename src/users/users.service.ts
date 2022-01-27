6fb87f80ddcc9c3ea235c6f4e7958fe85faacd57
Diego Hideky <diegohideky@gmail.com>
2022-01-27 16:55:05 -0300

    Added users crud

---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
+
+  create(createUserDto: CreateUserDto) {
+    const createdCat = new this.userModel(createUserDto);
+    return createdCat.save();
+  }
+
+  findAll() {
+    return this.userModel.find().exec();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id).exec();
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate({
+      _id: id
+    }, {
+      $set: updateUserDto
+    }, {
+      new: true
+    });
+  }
+
+  remove(id: string) {
+    return this.userModel.deleteOne({
+      _id: id,
+    }).exec();
+  }
+}
