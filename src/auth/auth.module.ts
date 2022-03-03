1e80d79b2aea3afce23c13ccb29f916f25605ffd
Diego Hideky <diegohideky@gmail.com>
2022-03-03 14:20:27 -0300

    Added configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,16 +5,20 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60m' },
+      secret: configuration().token.secret,
+      signOptions: { expiresIn: configuration().token.expiresIn },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
