2b20d4a84daee9f8ef708fe3c645acb6e313aab9
Lucas Felipe Santos Ceruti <66695066+lucasceruti@users.noreply.github.com>
2022-05-26 08:46:50 -0300

    Modify-teste

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable prettier/prettier*/
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
